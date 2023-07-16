7b6d7470107bb5a6cea0b5f195a4b23606a692ca
mumingpo <mumingpo@gmail.com>
2023-07-16 03:12:32 -0400

    added deployment scripts for sepolia test net

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.TEST_ACCOUNT_PRIVATE_KEY ?? ''],
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,11 +3,14 @@ import { ethers } from 'hardhat';
 async function main() {
     const storage = await ethers.deployContract('Storage');
     await storage.waitForDeployment();
-
     console.log(`Storage deployed at ${await storage.getAddress()}.`);
+    // sepolia test storage
+    // Storage deployed at 0x71E12D6d07C119c808958698530a5fEd9263C841.
+    // Storage deployed at 0xc078eA74dE8804dABcc408082428939c27ac37e9.
+    
 }
 
 main().catch((error) => {
-    console.error(error);
+    // console.error(error);
     process.exitCode = 1;
 });
